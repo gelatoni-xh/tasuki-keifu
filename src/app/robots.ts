ad5652d0d21208d83a4cd4febcfb54f2000f510c
cs_xuhuan <cs_xuhuan@163.com>
2026-07-29 02:11:17 +0800

    Point robots.txt to sitemap index

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: `${siteConfig.siteUrl}/sitemap.xml`,
+    sitemap: `${siteConfig.siteUrl}/sitemap-index.xml`,
     host: siteConfig.siteUrl,
   };
 }
